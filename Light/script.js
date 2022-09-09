7765b6bfd7e5a168aee44fe9894a0740463e8512
Marleni Colon <mcolon0291@gmail.com>
2022-09-09 15:12:37 -0400

    finished hw

--- Light/script.js
+++ Light/script.js
@@ -1,3 +1,36 @@
-function switch_On(){
-    document.getElementById('img').src= "lightoff.jpg"
-}
+
+let body = document.querySelector("body")
+let button = document.querySelector("button")
+
+let lightbulb = document.querySelector("img")
+
+
+// boolean variable 
+let lightOn = true
+
+
+button.addEventListener("click", function(event){
+
+    event.preventDefault();
+
+    if (lightOn) { 
+
+        lightOn = false;
+
+        button.textContent = "Turn On"; 
+        lightbulb.src = "lightoff.jpg"; 
+
+        body.style.backgroundColor = black;
+
+         
+        }
+    else { 
+        lightOn = true; 
+
+        button.textContent = "Turn Off"; 
+
+        lightbulb.src = "lighton.jpg"; 
+        body.style.backgroundColor = " dark grey";
+    
+        }
+})
