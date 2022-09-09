5d206801bde5919221271c67f91335013d5ea580
Marleni Colon <mcolon0291@gmail.com>
2022-09-09 12:13:21 -0400

    Updated light blub homework

--- Light/script.js
+++ Light/script.js
@@ -1,9 +1,3 @@
-function bulbToggle(){
-    let bulb = document.getElementById('lightbulb');
-
-    if(bulb.src.match('lightoff')){
-        bulb.src = 'lighton.jpg';
-    } else {
-        bulb.src = 'lightoff.jpg'
-    }
+function switch_On(){
+    document.getElementById('img').src= "lightoff.jpg"
 }
